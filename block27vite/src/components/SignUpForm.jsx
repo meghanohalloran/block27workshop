450bfe5194af65bae92c1d9e70cf175bcf57f44c
Meghan O'Halloran <meghaneohalloran@gmail.comuser.name meghanohallorangit pushcode>
2025-02-09 09:42:33 -0700

    Rest Authenticate Componens 8-17

--- block27vite/src/components/SignUpForm.jsx
+++ block27vite/src/components/SignUpForm.jsx
@@ -1,14 +1,13 @@
-import React, { useState } from "react";
+import { useState } from 'react';
 
-const SignUpForm = () => {
-const [username, setUsername] = useState("");
-const [password, setPassword] = useState("");
-const [error, setError] = useState(null);
+const SignUpForm = ({ setToken }) => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState(null);
 
-
-const handleSubmit = async (event) => {
-  event.preventDefault();
-      try {
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
       const response = await fetch('https://fsa-jwt-practice.herokuapp.com/signup', {
         method: 'POST',
         headers: {
@@ -23,35 +22,44 @@ const handleSubmit = async (event) => {
     } catch (err) {
       setError(err.message);
     }
-  
+  };
+
+  const handleClick = async () => {
+    try {
+      console.log("Authenticate button clicked");
+    } catch (err) {
+      setError(err.message);
+    }
+  };
+
+  return (
+    <div>
+      <h2>Sign Up</h2>
+      {error && <p>{error}</p>}
+      <form onSubmit={handleSubmit}>
+        <label>
+          Username:
+          <input 
+            type="text" 
+            value={username} 
+            onChange={(e) => setUsername(e.target.value)} 
+          />
+        </label>
+        <br />
+        <label>
+          Password:
+          <input 
+            type="password" 
+            value={password} 
+            onChange={(e) => setPassword(e.target.value)} 
+          />
+        </label>
+        <br />
+        <button type="submit">Submit</button>
+      </form>
+      <button onClick={handleClick}>Authenticate Token</button>
+    </div>
+  );
 };
 
-return (
-  <>
-  <h2>Sign Up</h2>
-  {error && <p>{error}</p>}
-  <form onSubmit={handleSubmit}>
-    <label>
-      Username:
-      <input 
-        type="text" 
-        value={username} 
-        onChange={(e) => setUsername(e.target.value)} 
-      />
-    </label>
-  
-    <label>
-      Password:
-      <input 
-        type="password" 
-        value={password} 
-        onChange={(e) => setPassword(e.target.value)} 
-      />
-    </label>
-    <br />
-    <button type="submit">Submit</button>
-  </form>
-</>
-);
-};
 export default SignUpForm;
