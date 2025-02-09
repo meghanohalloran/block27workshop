128134cb62283c92dfffa05fb3a063ac5e94f0db
Meghan O'Halloran <meghaneohalloran@gmail.comuser.name meghanohallorangit pushcode>
2025-02-09 09:02:18 -0700

    commit for 'authenticate component' 1-7

--- block27vite/src/components/SignUpForm.jsx
+++ block27vite/src/components/SignUpForm.jsx
@@ -8,35 +8,50 @@ const [error, setError] = useState(null);
 
 const handleSubmit = async (event) => {
   event.preventDefault();
-  console.log("Form submitted");
+      try {
+      const response = await fetch('https://fsa-jwt-practice.herokuapp.com/signup', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ username, password })
+      });
+      
+      const result = await response.json();
+      setToken(result.token);
+      console.log(result);
+    } catch (err) {
+      setError(err.message);
+    }
+  
 };
 
 return (
-  <div>
-    <h2>Sign Up</h2>
-    <form onSubmit={handleSubmit}>
-      <label>
-        Username:
-        <input 
-          type="text" 
-          value={username} 
-          onChange={(e) => setUsername(e.target.value)} 
-        />
-      </label>
-      <br />
-      <label>
-        Password:
-        <input 
-          type="password" 
-          value={password} 
-          onChange={(e) => setPassword(e.target.value)} 
-        />
-      </label>
-      <br />
-      <button type="submit">Submit</button>
-    </form>
-  </div>
-  );
+  <>
+  <h2>Sign Up</h2>
+  {error && <p>{error}</p>}
+  <form onSubmit={handleSubmit}>
+    <label>
+      Username:
+      <input 
+        type="text" 
+        value={username} 
+        onChange={(e) => setUsername(e.target.value)} 
+      />
+    </label>
+  
+    <label>
+      Password:
+      <input 
+        type="password" 
+        value={password} 
+        onChange={(e) => setPassword(e.target.value)} 
+      />
+    </label>
+    <br />
+    <button type="submit">Submit</button>
+  </form>
+</>
+);
 };
-
 export default SignUpForm;
